f5273d9c429426569e5db027aeecd0e4dd33606e
Tong Li <litong2358@gmail.com>
2025-10-10 23:22:14 -0400

    debug

--- privacy/dp_models.py
+++ privacy/dp_models.py
@@ -37,8 +37,8 @@ class BasicBlock(nn.Module):
     def forward(self, x):
         out = F.relu(self.gn1(self.conv1(x)))
         out = self.gn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
+        out = out + self.shortcut(x)   
+        out = self.relu(out)
         return out
 
 
